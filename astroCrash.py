aada45b20465e52f983c378d460814c53b1574eb
Judah Hudson <judahshudson@gmail.com>
2015-09-18 00:29:54 -0500

    02_Rotate Sprite

--- astroCrash.py
+++ astroCrash.py
@@ -1,22 +1,28 @@
-# Read Key
-# Demonstrates reading the keyboard
+# Rotate Sprite
+# Demonstrates rotating a sprite
 
 from livewires import games
 
 games.init(screen_width = 780,  screen_height = 480, fps = 50)
 
 class Ship(games.Sprite):
-    ''' A moving ship. '''
+    ''' A rotating ship. '''
     def update(self):
-        ''' Move ship based on keys pressed. '''
-        if games.keyboard.is_pressed(games.K_w):
-            self.y -= 1
-        if games.keyboard.is_pressed(games.K_s):
-            self.y += 1
-        if games.keyboard.is_pressed(games.K_a):
-            self.x -= 1
-        if games.keyboard.is_pressed(games.K_d):
-            self.x += 1
+        ''' Rotate based on keys pressed. '''
+        if games.keyboard.is_pressed(games.K_RIGHT):
+            self.angle += 1
+        if games.keyboard.is_pressed(games.K_LEFT):
+            self.angle -= 1
+
+        if games.keyboard.is_pressed(games.K_1):
+            self.angle = 0
+        if games.keyboard.is_pressed(games.K_2):
+            self.angle = 90
+        if games.keyboard.is_pressed(games.K_3):
+            self.angle = 180
+        if games.keyboard.is_pressed(games.K_4):
+            self.angle = 270
+
 
 def main():
     nebula_image = games.load_image('nebula.jpg', transparent = False)
@@ -31,3 +37,15 @@ def main():
     games.screen.mainloop()
 
 main()
+
+
+#class Ship(games.Sprite):
+##        ''' Move ship based on keys pressed. '''
+##        if games.keyboard.is_pressed(games.K_w):
+##            self.y -= 1
+##        if games.keyboard.is_pressed(games.K_s):
+##            self.y += 1
+##        if games.keyboard.is_pressed(games.K_a):
+##            self.x -= 1
+##        if games.keyboard.is_pressed(games.K_d):
+##            self.x += 1
